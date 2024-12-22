2fd7fcafcd92ad10bd39d82c2b075e86fc1f18ea
Bassant Aboelhassan <115794820+bass3fas@users.noreply.github.com>
2024-12-22 13:46:43 +0400

    Update index.ts with prop interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -7,3 +7,26 @@ export interface ButtonProps {
     label: string;
     onClick: () => void;
 }
+
+interface Address {
+    state: string;
+    city: string;
+    country: string;
+}
+
+interface Offers {
+    bed: string;
+    shower: string;
+    occupants: string;
+}
+
+export interface PropertyProps {
+    name: string;
+    address: Address;
+    rating: number;
+    category: string[];
+    price: number;
+    offers: Offers;
+    image: string;
+    discount: string;
+}

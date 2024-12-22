220ede11f649ee6161ca42b02c8e02b20387bf7f
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-22 15:15:14 +0400

    added the layout to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <Header />
       <div className="flex-grow flex items-center justify-center">
         <h1 className="text-2xl font-bold text-gray-800">Welcome to ALX Listing App</h1>
       </div>
-      <Footer />
     </div>
   );
 };

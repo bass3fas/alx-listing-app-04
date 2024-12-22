5b3d520e699839358c313be0d139574e3ae0f21a
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-22 14:01:40 +0400

    successful run

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,7 @@
+import { PropertyProps } from "@/interfaces";
 export const API_BASE_URL = 'https://api.example.com';
 export const DEFAULT_PLACEHOLDER_IMAGE = '/assets/placeholder.png';
-export const PROPERTYLISTINGSAMPLE: PropertyProps []  =  [
+export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
   {
     name: "Villa Ocean Breeze",
     address: {

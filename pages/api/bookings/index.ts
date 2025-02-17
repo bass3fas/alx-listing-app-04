f6db57a90b7b2d57d9dc415fcd4abc5ab797dcac
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2025-02-17 15:27:48 +0400

    install axios

---
+++ pages/api/bookings/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PROPERTYLISTINGSAMPLE } from "@/constants";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+
+    // Find the property in the sample data
+    const property = PROPERTYLISTINGSAMPLE.find(
+        (item) => item.id === Number(id)
+    );
+
+    if (!property) {
+        return res.status(404).json({ message: "Property not found" });
+    }
+
+    // Return the property data
+    res.status(200).json(property);
+}
